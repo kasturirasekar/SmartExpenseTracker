f9ace5b6653e1091bcad2721ca7446f62d4d377d
Kasturi Rasekar <kasturirasekar@gmail.com>
2025-09-10 08:28:21 +0530

    Update MainApp.java

---
+++ src/com/expenseTracker/MainApp.java
@@ -0,0 +1,10 @@
+package com.expenseTracker;
+
+import com.expenseTracker.UI.MainMenu;
+
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("🚀 Smart Expense Tracker Started...");
+        new MainMenu(); // Open Swing UI
+    }
+}
